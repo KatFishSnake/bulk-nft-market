b35fd5f40e9a2086857bbf66ce5f901010c062cb
KatFishSnake <andre.kuzac@gmail.com>
2022-07-01 14:04:12 -0700

    fix: adjusted logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,6 @@ import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
 import Seo from '@/components/Seo';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Vercel from '~/svg/Vercel.svg';
-
 export default function HomePage() {
   return (
     <Layout>
@@ -23,7 +14,7 @@ export default function HomePage() {
       <main>
         <section className='bg-white'>
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <Vercel className='text-5xl' />
+            <div className='text-6xl'>🍱</div>
             <h1 className='mt-4'>BNFT Marketplace</h1>
             <p className='mt-2 text-sm text-gray-800'>
               A start of the marketplace project
