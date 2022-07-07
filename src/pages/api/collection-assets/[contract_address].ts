f258cd604d0b1415637ced0e26f8de4055200464
KatFishSnake <andre.kuzac@gmail.com>
2022-07-07 09:57:54 -0700

    fix: attempt to fix fetching collection assets

--- src/pages/api/collection-assets/[contract_address].ts
+++ src/pages/api/collection-assets/[contract_address].ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader('Cache-Control', 's-maxage=86400');
   const { contract_address, order, limit } = req.query;
   const result = await fetch(
     `${process.env.OPENSEA_API_URL}/assets?asset_contract_address=${contract_address}&order_direction=${order}&offset=0&limit=${limit}&include_orders=false`
